0668ede80b16828a73fb91953456755ab3275a5d
Ivan Paredes <122500873+Critico0@users.noreply.github.com>
2023-01-31 19:14:11 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,326 @@
+const canvas = document.querySelector('canvas');
+const ctx = canvas.getContext('2d');
+
+canvas.width = 1024
+canvas.height = 576
+
+ctx.fillRect(0,0,canvas.width,canvas.height);
+
+const gravity = 0.7
+
+const background = new Sprite({
+    position:{
+        x:0,
+        y:0
+    },
+    imagesrc:"./img/background.png",
+})
+
+// Player1
+
+const player1 = new Figther ({
+    position: {x: 256, y: 100},
+    imagesrc:"./img/samurai/Idle.png",
+    frameMax: 8,
+    velocity: {x: 0, y: 0},
+    scale: 2.5,
+    offset:{
+        x: 250,
+        y: 165
+    },
+    sprites:{
+        idle:{
+            imagesrc:"./img/samurai/Idle.png",
+            frameMax: 8,
+        },
+
+        run:{
+            imagesrc:"./img/samurai/Run.png",
+            frameMax: 8,
+        },
+
+        runleft:{
+            imagesrc:"./img/samurai/Runleft.png",
+            frameMax: 8,
+        },
+
+        jump:{
+            imagesrc:"./img/samurai/Jump.png",
+            frameMax: 2,
+        },
+
+        fall:{
+            imagesrc:"./img/samurai/Fall.png",
+            frameMax: 2,
+        },
+        attack1:{
+            imagesrc:"./img/samurai/Attack1.png",
+            frameMax: 6,
+        }
+    },
+    attackbox:{
+        offset:{
+            x:120,
+            y:40
+        },
+        width:100,
+        height:50
+    }
+});
+ 
+
+// Opponent
+
+const opponent = new Figther({
+    position: {x: 656, y: 100},
+    imagesrc:"./img/monk/Idle.png",
+    frameMax: 10,
+    velocity: {x: 0, y: 0},
+    scale:2.5,
+    offset: {
+        x: 180,
+        y: 65
+    },
+    sprites:{
+        idle:{
+            imagesrc:"./img/monk/Idle.png",
+            frameMax: 10,
+        },
+
+        run:{
+            imagesrc:"./img/monk/Run.png",
+            frameMax: 8,
+        },
+
+        jump:{
+            imagesrc:"./img/monk/Jump.png",
+            frameMax: 3,
+        },
+
+        fall:{
+            imagesrc:"./img/monk/Fall.png",
+            frameMax: 3,
+        },
+        attack1:{
+            imagesrc:"./img/monk/Attack1.png",
+            frameMax: 7,
+        }
+    },
+    attackbox:{
+        offset:{
+            x:-245,
+            y:47
+        },
+        width:100,
+        height:65
+    }
+});
+
+// KeyBoard
+
+const keys = {
+    a:{
+        pressed: false
+    },
+    d:{
+        pressed: false
+    },
+    k:{
+        pressed: false
+    },
+    ArrowLeft:{
+        pressed: false
+    },
+    ArrowRigth:{
+        pressed: false
+    }
+
+
+}
+
+window.addEventListener('keydown',(event) =>{
+
+    switch(event.key){
+        case 'w':
+            player1.velocity.y = -20
+        break
+        case 'a':
+            keys.a.pressed = true
+            //player1.attackbox.offset.x = -50
+            player1.lastKey = 'a'
+        break
+        case 'd':
+            keys.d.pressed = true
+            //player1.attackbox.offset.x = 0
+            player1.lastKey = 'd'
+        break
+        case 'k':
+            player1.attack();
+        break
+                        
+    }
+
+    // Opponent Keys
+
+    switch(event.key){
+        case 'ArrowUp':
+            opponent.velocity.y = -20
+        break
+        case 'ArrowLeft':
+            keys.ArrowLeft.pressed = true
+            //opponent.attackbox.offset.x = -50
+            opponent.lastKey = 'ArrowLeft'
+        break
+        case 'ArrowRight':
+            keys.ArrowRigth.pressed = true
+            //opponent.attackbox.offset.x = 0
+            opponent.lastKey = 'ArrowRight'
+        break
+        case '0':
+            opponent.attack();
+        break
+                        
+    }
+});
+
+window.addEventListener('keyup',(event) =>{
+    switch(event.key){
+        case 'a':
+            keys.a.pressed = false
+        break
+        case 'd':
+            keys.d.pressed = false
+        break
+
+    }
+
+    // Opponent Keys
+    
+    switch(event.key){
+        case 'ArrowLeft':
+            keys.ArrowLeft.pressed = false
+        break
+        case 'ArrowRight':
+            keys.ArrowRigth.pressed = false
+        break
+                        
+    }
+});
+
+function determineWinner(player1, opponent){
+    clearTimeout(timerId)
+    document.querySelector('#displayText').style.display = 'flex'
+
+    if( player1.health === opponent.health ){
+            document.querySelector('#displayText').innerHTML = 'Tie'
+        }
+    else if( player1.health > opponent.health ){
+            document.querySelector('#displayText').innerHTML = 'Player 1 Wins'
+        }
+    else if(opponent.health > player1.health){
+            document.querySelector('#displayText').innerHTML = 'Player 2 Wins'
+        }
+}
+
+//Timer
+let timer = 61
+let timerId 
+
+function runtimer(){
+    if(timer> 0) {
+    timerId =setTimeout(runtimer, 1000)
+        timer--
+        document.querySelector('#timer').innerHTML = timer
+    }
+    if(timer === 0){
+        determineWinner(player1 , opponent, timerId);
+    }
+}
+
+// Colisiones
+
+function colision({obj1, obj2}){
+    return(
+        obj1.attackbox.position.x + obj1.attackbox.width >= obj2.position.x &&
+        obj1.attackbox.position.x <= obj2.position.x + obj2.width &&
+        obj1.attackbox.position.y + obj1.attackbox.height >= obj2.position.y &&
+        obj1.attackbox.position.y <= obj2.position.y + obj2.height
+    );
+}
+
+
+// Loop Animate
+
+function animate(){
+    window.requestAnimationFrame(animate);
+    ctx.fillStyle = 'black'
+    ctx.fillRect(0,0,canvas.width,canvas.height);
+    background.update();
+    player1.update();
+    opponent.update();
+
+    player1.velocity.x = 0
+    opponent.velocity.x = 0
+
+// keyboar movement Player
+
+    if(keys.a.pressed && player1.lastKey === 'a'){
+        player1.velocity.x = -5
+        player1.swichSprites('runleft')
+    }else if (keys.d.pressed && player1.lastKey === 'd'){
+        player1.velocity.x = +5
+        player1.swichSprites('run')
+    }else{
+        player1.swichSprites('idle')
+    }
+
+    if(player1.velocity.y < 0){
+       player1.swichSprites('jump')
+    }else if(player1.velocity.y > 0){
+        player1.swichSprites('fall')
+    }
+// keyboar movement Opponent
+    if(keys.ArrowLeft.pressed && opponent.lastKey === 'ArrowLeft'){
+        opponent.velocity.x = -5
+        opponent.swichSprites('run')
+        console.log(keys.ArrowLeft.pressed)
+    }else if (keys.ArrowRigth.pressed && opponent.lastKey === 'ArrowRight'){
+        opponent.velocity.x = +5
+        opponent.swichSprites('run')
+    }else{
+        opponent.swichSprites('idle')
+    }
+
+    if(opponent.velocity.y < 0){
+        opponent.swichSprites('jump')
+     }else if(opponent.velocity.y > 0){
+         opponent.swichSprites('fall')
+     }
+
+// Attack
+
+    if(colision({obj1 : player1, obj2 : opponent}) && player1.attacking === true){
+            player1.attacking = false;
+            console.log('Ataque');
+            opponent.health -= 20;
+            document.querySelector('#player2HealthBar').style.width = opponent.health + '%'
+    }
+    if(colision({obj1 : opponent, obj2 : player1}) && opponent.attacking === true){
+        opponent.attacking = false;
+        console.log('Opponent Hit');
+        player1.health -= 20;
+        document.querySelector('#player1HealthBar').style.width = player1.health + '%'
+}
+// Win
+
+    if(player1.health <=0 || opponent.health <=0){
+        determineWinner(player1, opponent, timerId);
+    }
+
+}
+
+animate();
+runtimer();
+
+
+
